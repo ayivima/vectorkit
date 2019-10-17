09e07ce63105bc72547b4c51ca0df6f8aa3b4a1d
ayivima <48770118+ayivima@users.noreply.github.com>
2019-10-17 09:37:14 +0000

    Changes for 0.1.5

--- test_vectorkit.py
+++ test_vectorkit.py
@@ -1,5 +1,5 @@
 
-"""Tests for Vectorkit."""
+"""Tests for Vectorkit"""
 
 from math import sqrt
 import sys
@@ -80,6 +80,24 @@ class VectorToolsTester(unittest.TestCase):
 	def test_component_membership(self):
 		self.assertTrue(2 in x)
 		self.assertFalse(4 in x)
+		
+	def test_corr(self):
+		a = Vector(2, 3, 5)
+		b = Vector(6, 5, 4)
+		c = round(a.corr(b), 2)
+		expected_c = -0.98
+
+		self.assertEqual(c, expected_c)
+
+	def test_cov(self):
+		a = Vector(2, 3, 5)
+		b = Vector(6, 5, 4)
+		c = Vector(3, 4, 5)
+		d = Vector(1, 1, 1)
+		z = a.cov(b), a.cov(c), a.cov(d)
+		expected_z = -1.0, 1.0, 0.0
+
+		self.assertEqual(z, expected_z)
 
 	def test_cross_product(self):
 		a = Vector(3, -3, 1)
@@ -126,7 +144,7 @@ class VectorToolsTester(unittest.TestCase):
 		# extension component.
 		# Additional test that vector 'a' is changed by in-place extension
 		a = Vector(1, 2)
-		a.extend(4)
+		a.pad(4)
 		expected_extended_a = Vector(1,2,0,0)
 
 		self.assertEqual(expected_extended_a, a) 
@@ -134,14 +152,14 @@ class VectorToolsTester(unittest.TestCase):
 
 		# Test in-place extension with custom extension component
 		a = Vector(1, 2)
-		a.extend(5, 3)
+		a.pad(5, 3)
 		expected_extended_a = Vector(1,2,3,3,3)
 
 		# Test for non in-place extension using .extended() :)
 		b = Vector(1,3)
 		
-		c = b.extended(4) # using default extension component
-		d = b.extended(4, 10)
+		c = b.padded(4) # using default extension component
+		d = b.padded(4, 10)
 
 		expected_c = Vector(1,3,0,0)
 		self.assertEqual(expected_c, c)
@@ -215,12 +233,12 @@ class VectorToolsTester(unittest.TestCase):
 
 		tsize = 0
 		props = (
-					a,
-					a.components,
-					a.dimensions,
-					a.min,
-					a.max,
-					a.sum
+			a,
+			a.components,
+			a.dimensions,
+			a.min,
+			a.max,
+			a.sum
 		)
 		for x in props:
 			tsize += sys.getsizeof(x)
@@ -294,6 +312,21 @@ class VectorToolsTester(unittest.TestCase):
 		a = randvec(5)
 
 		self.assertEqual(5, a.dimensions)
+	
+	def test_relu(self):
+		# Test positive and negative values
+		a = Vector(-1,-2,-3, 1)
+		b = a.relu()
+		expected_relu = Vector(0, 0, 0, 1)
+
+		self.assertEqual(b, expected_relu)
+		
+		# Test positive values
+		a = Vector(1, 2, 3, 1)
+		b = a.relu()
+		expected_relu = Vector(1, 2, 3, 1)
+		
+		self.assertEqual(b, expected_relu)
 
 	def test_shuffling(self):
 		# Test shuffled()
@@ -310,7 +343,30 @@ class VectorToolsTester(unittest.TestCase):
 		
 		a.shuffle()
 		self.assertTrue(a != b)
-
+		
+	def test_sigmoid(self):
+		
+		a = Vector(1,2,3)
+		b = a.sigmoid()
+		expected_sig = Vector(
+			0.7310585786300049,
+			0.8807970779778823,
+			0.9525741268224334
+		)
+		
+		self.assertEqual(b, expected_sig)
+		
+	def test_softmax(self):
+		
+		a = Vector(1,2,3)
+		b = a.softmax()
+		expected_soft = Vector(
+			0.09003057317038046,
+			0.24472847105479767,
+			0.6652409557748219
+		)
+		
+		self.assertEqual(b, expected_soft)
 
 	def test_std(self):
 		a = Vector(1,2,3,4,5)
@@ -413,7 +469,18 @@ class VectorToolsTester(unittest.TestCase):
 		self.assertEqual(expected_min, a.min)
 		self.assertEqual(expected_max, a.max)
 		self.assertEqual(expected_sum, a.sum)
-
+	
+	def test_tanh(self):
+		x = Vector(1,2,3)
+		tanh = x.tanh()
+		expected_tanh = Vector(
+			0.7615941559557646,
+			0.9640275800758169,
+			0.9950547536867307
+		)
+		
+		self.assertEqual(expected_tanh, tanh)
+	
 	def test_vector_equation(self):
 		a = Vector(1, 2, 3)
 		b = Vector(3, 2, 1)
